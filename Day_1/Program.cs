e8c38c77a37bcc475b77b823eee9d51490f12524
Fernando Duarte Pascoal <fernando.pascoal42@gmail.com>
2022-12-02 01:32:20 -0300

    Add project files.

---
+++ Day_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Get the Input file and read lines.
+
+string textFile = "../../../Input.txt";
+
+string[] lines = File.ReadAllLines(textFile);
+
+// Part One
+// Find the Elf carrying the most Calories.
+
+List<int> calories = new();
+
+int sum = 0;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    if (string.IsNullOrWhiteSpace(lines[i]))
+    {
+        calories.Add(sum);
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(lines[i]);
+    }
+}
+
+Console.WriteLine($"How many total Calories is that Elf carrying?\nR: {calories.Max()}");
+
+// Part Two
+// Find the top three Elves carrying the most Calories.
+
+int totalTopThreeCalories = calories.OrderByDescending(calories => calories).Take(3).Sum();
+
+Console.WriteLine($"How many Calories are those Elves carrying in total?\nR: {totalTopThreeCalories}");
